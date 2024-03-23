0223eb1ebe65be37c2de9c8ef52d28956f470c8a
FPC <felipepalmacastro@gmail.com>
2024-03-23 17:52:23 -0300

    Chapter Three Integration.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/felipepalma1/idiomatic
 
 go 1.22
+
+require github.com/felipepalma1/chapter-three-composite-types v0.0.0-20240323201939-7da1f8cf5616 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/felipepalma1/chapter-three-composite-types"
+)
 
 func main() {
 	fmt.Println("Idiomatic")
+	fmt.Println(chapter_three_composite_types.SimpleArray())
 }
